254e8a74023fbb84c8604a5eddc26c1608f22031
Dalton <daltongetbaker@gmail.com>
2024-03-19 18:22:46 -0400

    Fianl Day 2 commits

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -17,35 +17,7 @@ const studentSchema = new Schema({
   projects: Array,
 });
 
-// CREATE MODEL
-// The model() method defines a model (Book) and creates a collection (books) in MongoDB
-// The collection name will default to the lowercased, plural form of the model name:
-//                          "Book" --> "books"
 const Student = mongoose.model("Student", studentSchema);
  
 // EXPORT THE MODEL
 module.exports = Student;
-
-
-
-/*
-    {
-      "_id": 1,
-      "firstName": "Christine",
-      "lastName": "Clayton",
-      "email": "christine.clayton@example.com",
-      "phone": "567-890-1234",
-      "linkedinUrl": "https://linkedin.com/in/christineclaytonexample",
-      "languages": ["English", "Dutch"],
-      "program": "Web Dev",
-      "background": "Computer Engineering",
-      "image": "https://i.imgur.com/r8bo8u7.png",
-      "cohort": 1,
-      "projects": []
-    },
-
-
-
-
-
-*/
