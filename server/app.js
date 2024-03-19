b89b5839ebe4bb894957d6d105392fc72a2b8843
Dalton <daltongetbaker@gmail.com>
2024-03-19 17:06:14 -0400

    day 2 changes

--- server/app.js
+++ server/app.js
@@ -3,19 +3,15 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
+const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require('./routes/auth')
 var apiRouter = require('./routes/api')
+var docsRouter = require('./routes/docs');
 
 var app = express();
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
@@ -23,9 +19,23 @@ app.use(
   })
 );
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use('/auth', authRouter);
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
+app.use('/docs', docsRouter);
 
 module.exports = app;
